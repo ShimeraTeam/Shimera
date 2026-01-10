d3e5686f8a9f7719ca7738ffa8438ed7b1e96b77
paul.arbez <paul.arbez@epitech.eu>
2026-01-10 17:56:29 +0800

    ADD: compilation shared library

--- example/common/xmake.lua
+++ example/common/xmake.lua
@@ -4,6 +4,7 @@ add_requires("glew")
 
 set_languages("c++17")
 
+-- Librairie statique
 target("shimera")
     set_kind("static")
     add_files("*.cpp")
@@ -12,3 +13,15 @@ target("shimera")
     add_headerfiles("shimera.h")
     add_includedirs(".", {public = true})
     add_packages("glew", {public = true})
+
+-- Librairie shared
+target("shimera-shared")
+    set_kind("shared")
+    set_basename("shimera")
+    add_files("*.cpp")
+    add_headerfiles("*.h", "*.hpp")
+    add_headerfiles("uniform/*.hpp")
+    add_headerfiles("shimera.h")
+    add_includedirs(".", {public = true})
+    add_packages("glew", {public = true})
+    add_defines("SHIMERA_EXPORTS")
